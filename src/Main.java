389ab628221881b0b5a7e9cb4142c1e30a9aa755
shenghuodehezuo <40316354+shenghuodehezuo@users.noreply.github.com>
2019-01-24 14:21:30 +0800

    第二次提交

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
-        Properties pop=new Properties();
-        pop.
+    public static void main(String[] args)throws IOException , DocumentException {
+//        Properties pop=new Properties();
+//        Main.class.getResourceAsStream("resourses/test.properties");
+//        pop.load(new FileReader("resources/test.properties"));
+//        System.out.println(pop.getProperty("url"));
+        //XML读写
+        SAXReader reader=new SAXReader();
+        Document doc =reader.read(new File("resources/demo.xml"));
+        doc.getRootElement();
+        Element eml=doc.getRootElement();
+        eml.getText();
+        List<Element> list=eml.elements("student");
+        for (Element em:list){
+            System.out.println(em.element("name").getText());
+           System.out.println( em.element("age").getText());
+        }
+
     }
 }
